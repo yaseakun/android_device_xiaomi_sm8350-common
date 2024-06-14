4baa40841d48029ab3d007a477962dda25a3eca0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-14 23:52:51 +0800

    sm8350-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- common.mk
+++ common.mk
@@ -325,6 +325,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rro_overlays/config-vendor.xml:$(TARGET_COPY_OUT_VENDOR)/overlay/config/config.xml
 
+PRODUCT_PACKAGES += \
+    DeviceAsWebcamOverlaySM8350 \
+    NcmTetheringOverlay
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
